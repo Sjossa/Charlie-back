c2ce095bca5b7812fdf1cf0de79a8cb75ff0596b
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-08-25 10:58:31 +0200

    fix: port AND database for render

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 db.connect((er) => {
   if (er) {
-    console.log(er);
+    console.error("Erreur de connexion :", er);
   } else {
-    console.log("Vous etes connecter");
+    console.log("Vous êtes connecté à la base de données TiDB !");
   }
 });
